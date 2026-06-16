70b7d6a8c343c6fad371c09c5e8a9355fc1bfcc6
Ryan Miller <RyanMillerC@outlook.com>
2026-06-16 12:42:30 -0400

    Fix broswer caching backend images

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ def blue():
     images = [f for f in os.listdir(IMAGE_DIR) if f.startswith("blue-")]
     if not images:
         raise HTTPException(status_code=404, detail="No blue images found")
-    return FileResponse(os.path.join(IMAGE_DIR, random.choice(images)), media_type="image/png")
+    return FileResponse(os.path.join(IMAGE_DIR, random.choice(images)), media_type="image/png", headers={"Cache-Control": "no-store"})
 
 
 @app.get("/green")
@@ -21,4 +21,4 @@ def green():
     images = [f for f in os.listdir(IMAGE_DIR) if f.startswith("green-")]
     if not images:
         raise HTTPException(status_code=404, detail="No green images found")
-    return FileResponse(os.path.join(IMAGE_DIR, random.choice(images)), media_type="image/png")
+    return FileResponse(os.path.join(IMAGE_DIR, random.choice(images)), media_type="image/png", headers={"Cache-Control": "no-store"})
